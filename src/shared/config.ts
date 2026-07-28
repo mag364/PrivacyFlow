8c4a16aefe06031e86b70b4dbf5f4c0cbdc99567
Mike Gutierrez <mag364@gmail.com>
2026-07-28 16:28:16 -0400

    Release setup defaults update

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -4,7 +4,7 @@
 // -----------------------------------------------------------------------------
 
 export const APP_CONFIG = {
-  version: '1.0.2',
+  version: '1.0.3',
   productName: 'PrivacyFlow',
   tagline: 'DSR Tracking',
   updates: {
